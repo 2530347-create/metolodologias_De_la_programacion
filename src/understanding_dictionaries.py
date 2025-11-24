060838c795162d04632d4425c1db8b4f8da07cb9
Andrik Alexis García Obregón <2530347@upv.edu.mx>
2025-11-24 10:40:13 -0600

    dicionarios modificarlos y agregar cosas

--- src/understanding_dictionaries.py
+++ src/understanding_dictionaries.py
@@ -16,14 +16,40 @@ marge = {"color": "yellow",
 gun:0= {"scar": "yellow-orange", "headshot": 1.5}
 print(marge["headshot"])
 
+# Modifying an elements of a diccionari
+homer_0["color"]="blue"
 ## Add elements to a dictionary
 
 homer_0["x-psition"]=15
 homer_0["y-psition"]=25
 homer_0["z-psition"]=10
-print(homer_0)
+homer_0["name"]="omer"
 
 marge["x-psition"]=16
 marge["y-psition"]=26
 marge["z-psition"]=10
 print(homer_0)
+
+## Looping though items
+
+for key, value in homer_0.items():
+    print(f"The key {key} has value {value} ")
+
+
+for key, value in homer_0.key():
+    print(f"The key {key} has value {value} ")
+
+
+## Looping though key
+for key in homer_0.key():
+    print(key)
+
+
+## Looping though value
+for value in homer_0.value():
+    print(key)
+
+# NESTING
+# Listas de diccionarios
+# Listas en diccionarios
+# Diccionarios de diccionarios
