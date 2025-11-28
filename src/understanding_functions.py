81981bdf8f44ff99bb0f8a079629a1004da95fbd
Andrik Alexis García Obregón <2530347@upv.edu.mx>
2025-11-28 07:54:25 -0600

    funciones

--- src/understanding_functions.py
+++ src/understanding_functions.py
@@ -17,7 +17,40 @@ def gretting_Andrik():
     """
         funciona para saludar a una persona llama Andrik
     """
+"""
     for i in range(0,5):
         print("hola Andrik")
-
+"""
 gretting_Andrik()
+
+# Ejemplo de una funcion que genere el nombre completo de una persona y regresarlo
+
+
+def create_full_name(first_name, last_name, middle_name=""):
+    full_name = f"{first_name} {middle_name.strip()} {last_name.strip()}".title()
+    return full_name 
+
+first_name = input("Dame tu primer nombre: ")
+middle_name = input("Dame tu segundo nombre: (ENTER si no tinenes segundo nombre)")
+last_name = input("Dame tu apellido: ")
+
+# parametrso posicionales
+generate_fullname = create_full_name(
+    user_frist_name.strip().lower(),
+    user_last_name.strip().lower(),
+    user_middle_name.strip().lower())
+print(generate_fullname)
+
+# argumento llave
+generate_fullname_2 = create_full_name(
+middle_name = user_middle_name,
+first_name = user_frist_name,
+last_name = user_last_name)
+print(generate_fullname_2)
+
+"""
+manejo de datos (.txt, csv, json, excel, works, pdf)
+argas via consola (sys)
+click en python - comand line interface
+testing - casos prueba (borde, validos, invalidos)
+"""
